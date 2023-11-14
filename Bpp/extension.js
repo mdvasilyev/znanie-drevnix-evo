8bbdf7d0365316e23b66b3e6d178e2be59ebe2be
Maxim Vasilyev <mayxis@inbox.ru>
2023-11-14 13:19:20 +0300

    feat: add parser functionality

--- Bpp/extension.js
+++ Bpp/extension.js
@@ -22,23 +22,60 @@ function activate(context) {
 			vscode.window.showInformationMessage(newDate.toLocaleTimeString());
 		})
 	);
+	
+	function translateFile(editor, dictionary) {
+		editor.edit(editBuilder => {
+			const document = editor.document;
+			for (let lineIndex = 0; lineIndex < document.lineCount; lineIndex++) {
+				const line = document.lineAt(lineIndex);
+				const range = new vscode.Range(line.range.start, line.range.end);
+				if (line.text.startsWith('#include')) {
+					editBuilder.delete(range);
+				} else if (line.text.length >= 2) {
+					var splitted = line.text.match(/\b\w+\b|[.,\/#!$%\^&\*;:{}=\-_`~()]/g);
+					for (let i = 0; i < splitted.length; i++) {
+						if (splitted[i] in dictionary) {
+							splitted[i] = dictionary[splitted[i]];
+						}
+					}
+					editBuilder.replace(range, splitted.join(' '));
+				}
+			}
+			const must_have = '#include "../header/Ве_крест_крест.h"\n\nвнедрить хутор Русь;';
+			editBuilder.insert(document.lineAt(0).range.start, must_have);
+		});
+	}
 
-	context.subscriptions.push(  // основная функция
-		vscode.commands.registerCommand("Bpp.Translate", () => {
+	const myDict = {
+		'if': 'если',
+		'size_t': 'мерило',
+		'void': 'ничто'
+	};
+
+	context.subscriptions.push(vscode.commands.registerCommand('Bpp.Translate', () => {
 			const editor = vscode.window.activeTextEditor;
 			if (editor) {
-				const document = editor.document;
-				editor.edit(editBuilder => {
-					editor.selections.forEach(sel => {
-						const range = sel.isEmpty ? document.getWordRangeAtPosition(sel.start) || sel : sel;
-						let word = document.getText(range);
-						let transformed = word.split('').map(() => 'a').join(''); // прям как в хаскеле
-						editBuilder.replace(range, transformed);
-					})
-				})
+				translateFile(editor, myDict);
 			}
 		})
 	);
+
+	// context.subscriptions.push(  // основная функция
+	// 	vscode.commands.registerCommand("Bpp.Translate", () => {
+	// 		const editor = vscode.window.activeTextEditor;
+	// 		if (editor) {
+	// 			const document = editor.document;
+	// 			editor.edit(editBuilder => {
+	// 				editor.selections.forEach(sel => {
+	// 					const range = sel.isEmpty ? document.getWordRangeAtPosition(sel.start) || sel : sel;
+	// 					let word = document.getText(range);
+	// 					let transformed = word.split('').map(() => 'a').join(''); // прям как в хаскеле
+	// 					editBuilder.replace(range, transformed);
+	// 				})
+	// 			})
+	// 		}
+	// 	})
+	// );
 }
 
 function deactivate() { }
