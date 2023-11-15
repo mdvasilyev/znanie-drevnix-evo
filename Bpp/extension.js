a19f62aadbd5f94a4886c69603ba2e04641c3eee
Menzorov_Konstatin <mnz.const@gmail.com>
2023-11-15 19:53:44 +0300

    Final fuctionality

--- Bpp/extension.js
+++ Bpp/extension.js
@@ -11,23 +11,92 @@ function activate(context) {
 	const json = fs.readFileSync(filePath, 'utf8');
 	const dictionary = new Map(Object.entries(JSON.parse(json)))
 
+
 	/**
-	 * @param { vscode.TextDocument } document
+	 * @param {RegExpMatchArray} splittedLine
+	 * @param {number} [index]
 	 */
-	function parseCppCode(document) {
-		const docSize = document.lineCount;
-		for (let i = 0; i < docSize; i++) {
-			const line = document.lineAt(i);
-			const range = new vscode.Range(line.range.start, line.range.end);
-			var splitted = line.text.match(/[\p{L}\p{N}]+|[.,\/#!$%\^&\*;:{}=\-_`~()]/gu);
-			const s = splitted.forEach(elem => {
-				console.log("Current word: " + elem);
-				if (dictionary.has(elem)) {
-					elem = dictionary.get(elem);
-				}
-			});
-			console.log(splitted)
+	function skipWhitespace(splittedLine, index) {
+		index++;
+		while (/\s/.test(splittedLine[index])) {
+			index++;
 		}
+		return index;
+	}
+
+
+	/**
+	 * @param {RegExpMatchArray} splittedLine
+	 */
+	function concatSomeWords(splittedLine) {
+		let result = [];
+
+		const sl = splittedLine.length
+		for (let index = 0; index < sl; index++) {
+			if (splittedLine[index] === 'int') {
+				index = skipWhitespace(splittedLine, index);
+				if (splittedLine[index] === 'main') {
+					result.push('int main');
+				} else {
+					result.push('int');
+					result.push(' ');
+					result.push(splittedLine[index]);
+				}
+			} else if (splittedLine[index] === 'long') {
+				index = skipWhitespace(splittedLine, index);
+				if (splittedLine[index] === 'long') {
+					result.push('long long');
+				} else {
+					result.push('long');
+					result.push(' ');
+					result.push(splittedLine[index]);
+				}
+			} else {
+				result.push(splittedLine[index]);
+			}
+		}
+		
+		return result;
+	}
+
+	/**
+	 * @param {vscode.TextLine} line
+	 * @param {vscode.TextEditorEdit} editBuilder
+	 */
+	function parseLine(line, editBuilder) {
+		const range = new vscode.Range(line.range.start, line.range.end);
+		if (line.text.startsWith('#include')) {
+			editBuilder.delete(range);
+		} else if (!line.isEmptyOrWhitespace) {
+			const splittedLine = line.text.match(/[\p{L}\p{N}]+|[.,\/#!$%\^&\*;:{}=\-_`~()+*/%]|\s/gu);
+			
+			const lineAfterConcats = concatSomeWords(splittedLine);
+			
+			const replaced = lineAfterConcats.map((elem) => {
+				if (dictionary.has(elem)) {
+					return dictionary.get(elem);
+				}
+				return elem;
+			});
+
+			editBuilder.replace(range, replaced.join(''));
+		}
+	}
+
+
+	/**
+	 * @param {vscode.TextDocument} document
+	 * @param {vscode.TextEditorEdit} editBuilder
+	 */
+	function parseDocument(document, editBuilder) {
+		const documentLines = document.lineCount;
+		for (let lineIndex = 0; lineIndex < documentLines; lineIndex++) {
+			const line = document.lineAt(lineIndex);
+			parseLine(line, editBuilder);
+		}
+
+		const must_have = '#include "../header/Ве_крест_крест.h"';
+		editBuilder.insert(document.lineAt(0).range.start, must_have);
 	};
 
 	context.subscriptions.push(
@@ -35,30 +104,7 @@ function activate(context) {
 			const editor = vscode.window.activeTextEditor;
 			if (editor) {
 				editor.edit(editBuilder => {
-					const document = editor.document;
-					for (let lineIndex = 0; lineIndex < document.lineCount; lineIndex++) {
-						const line = document.lineAt(lineIndex);
-						const range = new vscode.Range(line.range.start, line.range.end);
-						if (line.text.startsWith('#include')) {
-							editBuilder.delete(range);
-						} else if (line.text.length >= 2) {
-							var splitted = line.text.match(/[\p{L}\p{N}]+|[.,\/#!$%\^&\*;:{}=\-_`~()]/gu);
-							const replaced = splitted.map((elem) => {
-								if (dictionary.has(elem)) {
-									return dictionary.get(elem);
-								}
-								return elem;
-							});
-							editBuilder.replace(range, replaced.join(' '));
-						}
-					}
-					const must_have = '#include "../header/Ве_крест_крест.h"';
-					editBuilder.insert(document.lineAt(0).range.start, must_have);
-				
-					if (document.lineAt(2).text != 'внедрить хутор Русь;' && document.lineAt(2).text != 'внедрить хутор Русь ;') {
-						const must_have = 'внедрить хутор Русь;\n';
-						editBuilder.insert(document.lineAt(2).range.start, must_have);
-					}
+					parseDocument(editor.document, editBuilder);
 				})
 			}
 		})
